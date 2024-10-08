a76826b15b910648b699965506e2c783c22693fa
Safwat Ahmed <safwatahmed669@gmail.com>
2024-10-08 03:32:16 +0300

    Initial commit

--- connect.php
+++
@@ -1,5 +0,0 @@
-<?php
-$conn = mysqli_connect('localhost', 'root', '', 'ShattablyDB');
-if (!$conn) {
-    die("Error Connecting to Database" . mysqli_connect_error());
-}
--- script.js
+++ script.js
@@ -6,10 +6,12 @@ document.getElementById("date").innerHTML = currentYear;
 // Back To Top Button
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () {
+  scrollFunction();
+};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 20) {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 1000) {
     document.getElementById("myBtn").style.display = "block";
   } else {
     document.getElementById("myBtn").style.display = "none";
@@ -22,6 +24,3 @@ function topFunction() {
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
 }
 
-
-
-// Reset Password 
